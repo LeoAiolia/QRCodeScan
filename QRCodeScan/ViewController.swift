6fad3c2fbbc6f68c5369b1e8051f6445d030ef01
run <a936381813@163.com>
2021-03-17 15:40:49 +0800

    create empty project

---
+++ QRCodeScan/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  QRCodeScan
+//
+//  Created by dev-yxr on 2021/3/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
